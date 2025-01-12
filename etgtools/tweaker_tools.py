72eb1fe9a47ab835a7370171b875788854550c16
lojack5 <1458329+lojack5@users.noreply.github.com>
2025-01-11 21:44:57 -0700

    Fixups for buildings docs (sphinx_generator): - Remove typehints from argsString before checking signature - lie and detect `_from`, `_is`, and `_def` as `from`, `is`, and `def for   signature validating purposes - Fix: don't remove const from identifiers containing const (eg: constraint)

--- etgtools/tweaker_tools.py
+++ etgtools/tweaker_tools.py
@@ -155,7 +155,8 @@ class FixWxPrefix(object):
 
         Finally, the 'wx.' prefix is added if needed.
         """
-        for txt in ['const', '*', '&', ' ']:
+        name = re.sub(r'(const(?![\w\d]))', '', name) # remove 'const', but not 'const'raints
+        for txt in ['*', '&', ' ']:
             name = name.replace(txt, '')
         name = name.replace('::', '.')
         if not is_expression:
